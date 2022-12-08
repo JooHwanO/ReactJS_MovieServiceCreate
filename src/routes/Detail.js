adca074a2d426b06b9097cb3a85d991b8cf3ac6c
JooHwanO <jho-28@nate.com>
2022-12-08 23:40:20 +0900

    비동기 에러 처리

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,19 +1,23 @@
-import { useEffect } from "react";
+import { useState,useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 function Detail()
 {
+    const [Item, setItem] = useState();
     const {id} = useParams();
     console.log(id);
-    const getMovie = async()=>{
-        const json = await(await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)).json()
+    const getMovie = (async()=>{
+        const json= await (await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id= ${id}`)).json();
         console.log(json);
-    };
+        setItem(json);
+    });
 
     useEffect(()=>{
         getMovie();
-    },); //[] 안넣고?
+        //워링무시
+// eslint-disable-next-line react-hooks/exhaustive-deps
+    },[]); //[] 안넣고?
     
-    return(<h1>Detail</h1>);
+    return(<h1>{Item.data.movie.title}</h1>);
 }
 export default Detail;
