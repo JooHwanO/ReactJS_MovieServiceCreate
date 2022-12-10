22ce32f5d7e043b934540023b21f3a2c023b0867
JooHwanO <jho-28@nate.com>
2022-12-10 20:32:31 +0900

    오류 설정

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -6,7 +6,7 @@ function Detail()
     const [Item, setItem] = useState();
     const {id} = useParams();
     console.log(id);
-    const getMovie = useCallback(async()=>{
+    const getMovie = (async()=>{
         const json= await (await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id= ${id}`)).json();
         console.log(json);
         setItem(json);
@@ -16,7 +16,7 @@ function Detail()
         getMovie();
         //워링무시
 // eslint-disable-next-line react-hooks/exhaustive-deps
-    },[id]); 
+    },[]); 
     
     return(<h1>{Item.data.movie.title}</h1>);
 }
